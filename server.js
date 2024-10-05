69e955709205592cfba1ecbd04f7432b7bf0c955
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-10-04 17:14:22 -0700

    add more routes
    - add routes to get: individual users, all posts, individual posts
    - add route to delete an item by item_id
    - add route to update an existing item
    - add route to add an item to db

--- server.js
+++ server.js
@@ -44,6 +44,20 @@ app.get("/users", (req, res) => {
     });
 });
 
+// route to get a specific user by user_id
+app.get("/users/:user_id", (req, res) => {
+    const { user_id } = req.params;
+    const query = `SELECT * FROM users WHERE user_id = ?`;
+
+    db.query(query, [user_id], (err, results) => {
+        if (err) {
+            throw err;
+        }
+
+        res.json(results[0]);
+    });
+});
+
 // route to add a new user
 app.post("/signup", (req, res) => {
     const { email, password, phone_number, address, access_level } = req.body;
@@ -115,7 +129,7 @@ app.get("/item/:item_id", (req, res) => {
 
     db.query(query, [itemId], (err, results) => {
         if (err) {
-            console.error("Error fetching items:", err);
+            console.error("Error fetching item:", err);
             return res.status(500).json({ error: "Failed to fetch item" });
         }
 
@@ -127,6 +141,88 @@ app.get("/item/:item_id", (req, res) => {
         res.status(200).json(results[0]);
     });
 });
+
+// route to add an item to db
+app.post("/item", (req, res) => {
+    const { name, value, transfer_cost } = req.body;
+    const query = `INSERT INTO item (name, value, transfer_cost) VALUES (?, ?, ?)`;
+
+    db.query(query, [name, value, transfer_cost], (req, res) => {
+        if (err) {
+            console.error("Error adding item:", err);
+            return res.status(500).json({ error: "Failed to add item" });
+        }
+
+        // confirm item was created
+        res.json({
+            message: "Item created",
+            item_id: result.insertId,
+        });
+    });
+});
+
+// route to update an existing item
+app.put("/item/:item_id", (req, res) => {
+    const { item_id } = req.params;
+    const { name, value, transfer_cost } = req.body;
+    const query = `UPDATE item SET name = ?, value = ?, transfer_cost = ? WHERE item_id = ?`;
+
+    db.query(query, [name, value, transfer_cost, item_id], (err, result) => {
+        if (err) {
+            console.error("Error updating item", err);
+            return res.status(500).json({ error: "Failed to update item" });
+        }
+
+        res.json({ message: "Item updated" });
+    });
+});
+
+// route to delete an item
+app.delete("item/:item_id", (req, res) => {
+    const { item_id } = req.params;
+    const query = `DELETE FROM item WHERE item_id = ?`;
+
+    db.query(query, [item_id], (err, result) => {
+        if (err) {
+            console.error("Error deleting item", err);
+            return res.status(500).json({ error: "Failed to delete item" });
+        }
+
+        res.json({ message: "Item deleted" });
+    });
+});
+
+// route to get all posts
+app.get("/posts", (req, res) => {
+    const query = `SELECT * FROM post`;
+
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error("Error fetching all posts", err);
+            return res.status(500).json({ error: "Failed to fetch all posts" });
+        }
+
+        // return the posts
+        res.json(results);
+    });
+});
+
+// route to get a specific post
+app.get("/posts/:post_id", (req, res) => {
+    const { post_id } = req.params;
+    const query = `SELECT * FROM post WHERE post_id = ?`;
+
+    db.query(query, [post_id], (err, results) => {
+        if (err) {
+            console.error("Error fetching post", err);
+            return res.status(500).json({ error: "Failed to fetch post" });
+        }
+
+        res.json(results[0]);
+    });
+});
+
+// TODO Add a post, delete a post,
 
 // start the server
 app.listen(port, "0.0.0.0", () => {
